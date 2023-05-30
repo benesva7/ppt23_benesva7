d89d9ce92f188a107edcd1bcc11980e1253b2c33
Václav Beneš <benesva7@cvut.cz>
2023-05-30 21:42:13 +0200

    Test2

---
+++ Ppt23.Api/Data/Pracovnik.cs
@@ -0,0 +1,14 @@
+﻿namespace Ppt23.Api.Data
+{
+    public class Pracovnik
+    {
+            public Guid Id { get; set; }
+
+            public string Name { get; set; } = string.Empty;
+
+            public string JobTitle { get; set; } = string.Empty;
+
+            public List<Ukony> Ukons { get; set; } = new();
+        
+    }
+}
